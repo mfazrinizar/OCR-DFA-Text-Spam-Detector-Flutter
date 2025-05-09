1f6f4cda4edd05517a4942a8f3ea6b9cdd9a407e
M. Fazri Nizar <anon6372098.id@gmail.com>
2025-05-09 19:31:15 +0700

    Init

---
+++ README.md
@@ -0,0 +1,3 @@
+# ocr_dfa_text_spam_filter
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/ocr_dfa_text_spam_filter/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.ocr_dfa_text_spam_filter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
